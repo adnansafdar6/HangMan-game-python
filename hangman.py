6ff2bb104493fd12bdb58f9fb4cbd1a7aaf77fc7
Adnan Safdar <adnanmehar969@gmail.com>
2024-12-14 19:47:42 +0500

    pdate Code

--- hangman.py
+++ hangman.py
@@ -1,43 +1,44 @@
-sting='hang man game'
-print(f"Welcome to the {sting.title()}!")
-
-word='apple'
 def word_array(word):
-    d=['-']
-    arr=[]
-    for i in range(len(word)):
-       arr.extend(d)
-    return arr
+    return ['-'] * len(word)
 
 def find_all_indexes(string, char):
     return [i for i, c in enumerate(string) if c == char]
 
 def replace_at_indexes(string, indexes, letter):
     return [letter if i in indexes else char for i, char in enumerate(string)]
+
 def match_letters(word):
     count = 0
-    guesses=6
-    arr=word_array(word)
+    guesses = 6
+    arr = word_array(word)
+    hangman_parts = ['o', '/', '\\', '|', '/', '\\']
+    
     while count < guesses: 
-      letter=input('Please Enter the word: ')
-      if letter in word:
-         ind=find_all_indexes(word, letter)
-         arr=replace_at_indexes(arr, ind, letter)
-       
-      else:
-          count+=1
-          print(f'wrong guess ,lifes will be remaing {count}/{guesses}')
-        #   hangman='o/\|/\ '
-        #   for c in hangman(range(count)):
-              
-              
-             
-      print(arr)
-      if '-' not in arr:
-          print('You Won')
-          break
+        print(f"Current word: {' '.join(arr)}")
+        letter = input('Please Enter a letter: ').lower()
+        
+        if len(letter) != 1 or not letter.isalpha():
+            print("Please enter only a single letter.")
+            continue
+        
+        if letter in word:
+            ind = find_all_indexes(word, letter)
+            arr = replace_at_indexes(arr, ind, letter)
+        else:
+            count += 1
+            print(f'Wrong guess. Lives remaining: {guesses - count}/{guesses}')
+            for i in range(count):
+                print(hangman_parts[i % len(hangman_parts)]) 
+        
+        if '-' not in arr:
+            print(f"Congratulations! You've guessed the word: {''.join(arr)}")
+            break
     else:
-        print('You Lost')
+        print(f"You lost! The word was: {word}")
+
+
+word = input('Please Enter a word:')
+print(f"Welcome to the Hangman Game!")
 match_letters(word)
-print(word)
+
   
